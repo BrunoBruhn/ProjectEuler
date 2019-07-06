9734d1041071208d53035209f2118e680ea132be
julian.bruhn <julian.bruhn@rwth-aachen.de>
2019-07-06 18:52:00 +0200

    finished 11

--- 11.py
+++ 11.py
@@ -1,5 +1,6 @@
 
-all_list_objects=[[8,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,8],
+all_list_objects=\
+[[8,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,8],
 [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00],
 [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65],
 [52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91],
@@ -20,30 +21,81 @@ all_list_objects=[[8,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,8],
 [20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54],
 [01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48]]
 
-def check_horizontally(list_object):
+def check_horizontal():
+	def hor_check(list_object):
+		incumbent=0
+		for i in range(len(list_object)-4):
+			product=1
+			for n in range(4):
+				product=product*list_object[i+n]
+				
+			if product>incumbent:
+				incumbent=product
+				
+		return incumbent
+
 	incumbent=0
-	for i in range(len(list_object)-4):
-		product=1
-		for n in range(4):
-			product=product*list_object[i+n]
-		if product>incumbent:
-			incumbent=product
+
+	for i in range(len(all_list_objects)):
+		if hor_check(all_list_objects[i])>= incumbent:
+			incumbent=hor_check(all_list_objects[i])
+
 	return incumbent
 
-#def check_vertically(list_object):
+
+def check_vertically():
+	incumbent=0
+	for i in range(len(all_list_objects[0])):
+		
+		for j in range(len(all_list_objects)-3):
+			product=1
+			for n in range(4):
+				product=product*all_list_objects[j+n][i]
+
+			if product>incumbent:
+				incumbent=product
+
+	return incumbent
 	
 # \\\
-#def check_diagonally_1(list_object):
+def check_diagonally_1():
 
-# ///
-#def check_diagonally_2(list_object):
+	incumbent=0
+	for i in range(len(all_list_objects[0])-3):
+		
+		for j in range(len(all_list_objects)-3):
+			product=1
+			for n in range(4):
+				product=product*all_list_objects[j+n][i+n]
+
+			if product>incumbent:
+				incumbent=product
+
+	return incumbent
+
+def check_diagonally_2():
+
+	incumbent=0
+	for i in range(3,len(all_list_objects[0])):
+		
+		for j in range(len(all_list_objects)-3):
+			product=1
+			for n in range(4):
+				product=product*all_list_objects[j+n][i-n]
+	
+			if product>incumbent:
+				incumbent=product
+
+	return incumbent
+
+results=[]
+
+results.append(check_horizontal())
+results.append(check_vertically())
+results.append(check_diagonally_1())
+results.append(check_diagonally_2())
+
+print max(results)
 
 
-incumbent=0
 
-for i in range(len(all_list_objects)):
-	if check_horizontally(all_list_objects[i])>= incumbent:
-		incumbent=check_horizontally(all_list_objects[i])
-
-
-print incumbent
