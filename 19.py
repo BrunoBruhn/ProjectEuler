cdd092bff7b6c90d9a9e5178a4117288a2512715
julian.bruhn <julian.bruhn@rwth-aachen.de>
2019-07-07 16:12:19 +0200

    finished 19

--- 19.py
+++ 19.py
@@ -1,14 +1,15 @@
+
 import calendar
 
 counter=0
-for y in range(1,11):
+for y in range(100):
 	
-	year=1990+y
+	year=1901+y
 
 	for m in range(1,13):
 		
 		localtime = calendar.weekday(year,m,1)
-
+	
 		if localtime==6:
 			counter+=1
 
