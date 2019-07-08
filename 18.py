974901afe0b5aa8ee7feaa4d7763eddc3b4558fc
julian.bruhn <julian.bruhn@rwth-aachen.de>
2019-07-08 23:39:44 +0200

    18 done

--- 18.py
+++ 18.py
@@ -1,29 +1,36 @@
+pyramid=[\
+[75],\
+[95,64],\
+[17,47,82],\
+[18,35,87,10],\
+[20,4,82,47,65],\
+[19,1,23,75,3,34],\
+[88,2,77,73,7,63,67],\
+[99,65,4,28,6,16,70,92],\
+[41,41,26,56,83,40,80,70,33],\
+[41,48,72,33,47,32,37,16,94,29],\
+[53,71,44,65,25,43,91,52,97,51,14],\
+[70,11,33,28,77,73,17,78,39,68,17,57],\
+[91,71,52,38,17,14,91,43,58,50,27,29,48],\
+[63,66,4,68,89,53,67,30,73,16,69,87,40,31],\
+[4,62,98,27,23,9,70,98,73,93,38,53,60,4,23]]
 
-N = 3
-  
-# Function for finding maximum sum 
-def maxPathSum(tri, m, n): 
-  
-    # loop for bottom-up calculation 
-    for i in range(m-1, -1, -1): 
-        for j in range(i+1): 
-  
-            # for each element, check both 
-            # elements just below the number 
-            # and below right to the number 
-            # add the maximum of them to it 
-            if (tri[i+1][j] > tri[i+1][j+1]): 
-                tri[i][j] += tri[i+1][j] 
-            else: 
-                tri[i][j] += tri[i+1][j+1] 
-  
-    # return the top element 
-    # which stores the maximum sum 
-    return tri[0][0] 
-  
-# Driver program to test above function 
-  
-tri = [[1, 0, 0], 
-       [4, 8, 0], 
-       [1, 5, 3]] 
-print(maxPathSum(tri, 2, 2)) 
+#turn pyramid on its head
+pyramid_turned=[]
+for i in range(len(pyramid)):
+	pyramid_turned.append(pyramid[-i-1])
+
+for stufe in range(1,len(pyramid)):
+
+	for index in range(len(pyramid_turned[stufe])):
+
+		if pyramid_turned[stufe-1][index]>pyramid_turned[stufe-1][index+1]:
+			pyramid_turned[stufe][index]=pyramid_turned[stufe-1][index]+pyramid_turned[stufe][index]
+
+		else:
+			pyramid_turned[stufe][index]=pyramid_turned[stufe-1][index+1]+pyramid_turned[stufe][index]
+		if stufe==14:
+			top_nr=pyramid_turned[-1][-1]
+		
+
+print (top_nr)
